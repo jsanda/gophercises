0eec3fb22e60607612278c7a6d97116e13195e0e
John Sanda <john.sanda@gmail.com>
2018-08-08 22:46:01 -0400

    initial support for timed quiz
    There is now a -time flag which defaults to 60 seconds. When the time is
    up, the quiz is ended regardless of whether or not it is completed. The
    number right/wrong is printed and the program terminates. If you finish
    the quiz before the time is up, the number right/wrong is print and the
    program terminates.

--- exercises/quiz.go
+++ exercises/quiz.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"strings"
 	"flag"
+	"time"
 )
 
 type quiz struct {}
@@ -19,17 +20,43 @@ func NewQuiz() *quiz {
 func (*quiz) Run() error {
 	fileName := flag.String("quiz", "exercises/problems.csv",
 		"Specifies the quiz which should be a CSV file")
+	seconds := flag.Int("time", 60,
+		"Specifies the time in seconds allowed to complete the quiz")
 
 	flag.Parse()
 
+	var right, wrong int32
+
+	fmt.Printf("You have %d seconds to complete the quiz. Go!\n", *seconds)
+	timer := time.NewTimer(time.Duration(*seconds) * time.Second)
+	done := make(chan struct{})
+	errors := make(chan error)
+
+	go takeQuiz(fileName, &right, &wrong, done, errors)
+
+	select {
+	case <-timer.C:
+		fmt.Println("\nTime is up!")
+		fmt.Printf("\nright: %d, wrong: %d\n", right, wrong)
+	case <-done:
+		fmt.Println("Finished!")
+		fmt.Printf("\nright: %d, wrong: %d\n", right, wrong)
+	case err := <- errors:
+		return err
+	}
+
+	return nil
+}
+
+func takeQuiz(fileName *string, right *int32, wrong *int32, done chan<- struct{}, errors chan<- error) {
 	file, err := os.Open(*fileName)
 	if err != nil {
-		return fmt.Errorf("Failed to open %s: %s", *fileName, err)
+		errors <- fmt.Errorf("Failed to open %s: %s", *fileName, err)
+		return
 	}
 
 	reader := bufio.NewReader(file)
 	inputReader := bufio.NewReader(os.Stdin)
-	var right, wrong int32
 
 	r := csv.NewReader(reader)
 	r.FieldsPerRecord = 2
@@ -40,7 +67,8 @@ func (*quiz) Run() error {
 			break
 		}
 		if err != nil {
-			return fmt.Errorf("Failed to parse %s: %s", *fileName, err)
+			errors <- fmt.Errorf("Failed to parse %s: %s", *fileName, err)
+			return
 		}
 		question := row[0]
 		answer := row[1]
@@ -49,17 +77,16 @@ func (*quiz) Run() error {
 		input, err := inputReader.ReadString('\n')
 
 		if err != nil {
-			return fmt.Errorf("Failed to read input: %s", err)
+			errors <- fmt.Errorf("Failed to read input: %s", err)
+			return
 		}
 
 		if strings.TrimRight(input, "\n") == answer {
-			right++
+			*right++
 		} else {
-			wrong++
+			*wrong++
 		}
 	}
 
-	fmt.Printf("\nright: %d, wrong: %d\n", right, wrong)
-
-	return nil
+	done <- struct{}{}
 }
